29f3242821ac44a74f9749ce1bbaba3cc45da5c1
Susu-kr <57660360+Susu-kr@users.noreply.github.com>
2021-07-06 16:25:04 +0900

    AVL Remove ++

--- AVL_TREE/AVL_TREE/AVL.cpp
+++ AVL_TREE/AVL_TREE/AVL.cpp
@@ -2,20 +2,14 @@
 
 int AVL::GetHeight(Node * node)
 {
-	if (node == NULL) return 0;
-	
+	if (node == NULL) return -1;
 	return node->height;
 }
 
 void AVL::SetHeight(Node * node)
 {
-	int h = 0;
-	if (node != NULL) {
-		int left = GetHeight(node->left);
-		int right = GetHeight(node->right);
-		h = GetMax(GetHeight(node->left), GetHeight(node->right)) + 1;
-	}
-    node->height = h;
+	if (node == NULL) { return; }
+	node->height = GetMax(GetHeight(node->left), GetHeight(node->right)) + 1;
 }
 
 int AVL::GetDiff(Node * node)
@@ -23,30 +17,62 @@ int AVL::GetDiff(Node * node)
 	if (node == NULL) return 0;
 	return GetHeight(node->left) - GetHeight(node->right);
 }
-
+/* LL(Y rotates to the left):
+		k2                       k1
+	   /  \                     /  \
+	  X    k1         ==>      k2   Z
+		  /  \                /  \
+		 Y    Z              X    Y
+ */
 Node * AVL::LL(Node * node)
 {
 	Node * leftChild = node->left;
 	node->left = leftChild->right;	// 해당 노드의 왼쪽자식 = 왼쪽자식의 오른쪽자식
 	leftChild->right = node;		// 왼쪽자식의 오른쪽자식 = 해당 노드
+	SetHeight(node);
+	SetHeight(leftChild);
 	return leftChild;
 }
-
+/* RR(Y rotates to the right):
+		k2                   k1
+	   /  \                 /  \
+	  k1   Z     ==>       X   k2
+	 / \                      /  \
+	X   Y                    Y    Z
+*/
 Node * AVL::RR(Node * node)
 {
 	Node * rightChild = node->right;
 	node->right = rightChild->left; // 해당 노드의 오른쪽자식 = 오른쪽자식의 왼쪽자식
 	rightChild->left = node;		 // 오른쪽자식의 왼쪽자식 = 해당 노드
+	SetHeight(node);
+	SetHeight(rightChild);
 	return rightChild;
 }
-
+/* LR(B rotates to the left, then C rotates to the right):
+	  k3                         k3                       k2
+	 /  \                       /  \                     /  \
+	k1   D                     k2   D                   k1   k3
+   /  \         ==>           /  \        ==>          / \   / \
+  A    k2                    k1   C                   A  B  C   D
+	  /  \                  /  \
+	 B    C                A    B
+*/
 Node * AVL::LR(Node * node)
 {
 	Node * leftChild = node->left;
 	node->left = RR(leftChild);	 // RR 수행 후 LL 수행
 	return LL(node);
 }
-
+/* RL(D rotates to the right, then C rotates to the left):
+	   k3                         k3                          k2
+	  /  \                       /  \                        /  \
+	 A    k1                    A    k2                     k3   k1
+		 /  \       ==>             /  \         ==>       /  \  / \
+		k2   B                     C    k1                A   C D   B
+	   /  \                            /  \
+	  C    D                          D    B
+ */
 Node * AVL::RL(Node * node)
 {
 	Node * rightChild = node->right;
@@ -57,22 +83,19 @@ Node * AVL::RL(Node * node)
 Node * AVL::balance(Node * node)
 {
 	if (node == NULL) return NULL;
-	node->left = balance(node->left);
-	node->right = balance(node->right);
 	int diff = GetDiff(node);
-	if (diff > 1) { // LL LR
-		if (GetDiff(node->left) > 0)
+	if (diff == 2) { // LL LR
+		if (GetDiff(node->left) == 1)
 			node = LL(node);
 		else
 			node = LR(node);
 	}
-	else if (diff < -1) { // RR RL
-		if (GetDiff(node->right) <= -1)
+	else if (diff == -2) { // RR RL
+		if (GetDiff(node->right) == -1)
 			node = RR(node);
 		else
 			node = RL(node);
 	}
-	SetHeight(node);
 	return node;
 }
 
@@ -92,7 +115,7 @@ Node * AVL::search(Node * node, int num)
 	return nullptr;
 }
 
-bool AVL::insert_Recursive(Node *& node, int num, string name)
+Node* AVL::insert_Recursive(Node *& node, int num, string name)
 {
 	if (node == NULL) {
 		Node * temp = new Node();
@@ -113,57 +136,44 @@ bool AVL::insert_Recursive(Node *& node, int num, string name)
 	}
 	else {
 		cout << "데이터 중복" << endl;
-		return false;
+		return node;
 	}
-	return true;
+	SetHeight(node);
+	return node;
 }
 
-bool AVL::insert(int num, string name)
+void AVL::insert(int num, string name)
 {
-	return insert_Recursive(this->root, num, name);
+	root = insert_Recursive(this->root, num, name);
 }
 
-Node * AVL::remove_Recursive(Node * node, Node * parent, int num)
+Node * AVL::remove_Recursive(Node * node, int num)
 {
+	Node * temp = node;
 	if (node == NULL) return NULL;
-	Node * removeNode = NULL;
-	if (num < node->num) {
-		removeNode = remove_Recursive(node->left, node, num);
+	if (num < node->num) node->left = remove_Recursive(node->left, num);
+	else if (num > node->num) node->right = remove_Recursive(node->right, num);
+	// 3. 삭제하려는 노드의 자식 서브트리가 2인 경우
+	else if (node->left != NULL && node->right != NULL) {
+		// 삭제될 노드의 왼쪽 서브트리보다 크고, 삭제될 노드의 오른쪽 서브트리보단 작아야함
+		Node * min = minNode(node->right);
+		node->num = min->num;
+		node->name = min->name;
+		node->left = remove_Recursive(node->left, min->num);
 	}
-	else if (num > node->num) {
-		removeNode = remove_Recursive(node->right, node, num);
+	else {
+		if (node->left == NULL) node = node->right;
+		else if (node->right == NULL) node = node->left;
+		delete temp;
 	}
-	else if(node->num == num){
-		removeNode = node;
-		if (node->left == NULL && node->right == NULL) {
-			if (parent->left == node) parent->left = NULL;
-			if (parent->right == node) parent->right = NULL;
-		}
-		if (node->left == NULL || node->right == NULL) {
-			// 자식 둘 중 하나가 NULL
-			Node* temp = NULL;
-			if (node->left != NULL) temp = node->left; // 왼쪽 자식이 NULL
-			else temp = node->right;
-
-			if (parent->left == node) parent->left = temp;
-			else parent->right = temp; // 오른쪽 자식이 NULL
-		}
-		// 3. 삭제하려는 노드의 자식 서브트리가 2인 경우
-		else if (node->left != NULL && node->right != NULL) {
-			// 삭제될 노드의 왼쪽 서브트리보다 크고, 삭제될 노드의 오른쪽 서브트리보단 작아야함
-			Node * min = minNode(node->right);
-			min = remove_Recursive(node, NULL, min->num);
-			node->num = min->num;
-		}
-	}
-	return removeNode;
+	SetHeight(node);
+	node = balance(node);
+	return node;
 }
 
-Node * AVL::remove(int num)
+void AVL::remove(int num)
 {
-	Node * temp = remove_Recursive(root, NULL, num);
-	balance(root);
-	return temp;
+	root = remove_Recursive(this->root, num);
 }
 
 void AVL::Show_Recursive(Node * node, int level)
@@ -171,12 +181,8 @@ void AVL::Show_Recursive(Node * node, int level)
 	if (node != NULL) {
 		Show_Recursive(node->right, level + 1);
 		cout << "\n";
-		if (node == root) {
-			cout << "Root : ";
-		}
-		for (int i = 0; i < level && node != root; i++) {
-			cout << "\t";
-		}
+		if (node == root) cout << "Root : ";
+		for (int i = 0; i < level && node != root; i++)	cout << "\t";
 		cout << node->num << "," << node->name << "(" << node->height << ")\t";
 		Show_Recursive(node->left, level + 1);
 	}
@@ -219,4 +225,3 @@ void AVL::postorder(Node * node)
 	postorder(node->right);
 	cout << node->num << "," << node->name << " ";
 }
-
